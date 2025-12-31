0c87ae2fb1562d970fbb3ca48ffe2115e06f38e3
rangeset <Rangeset24@outlook.com>
2025-12-31 22:57:48 +0800

    gui(proxy): add proxy configuration

--- src/client/java/org/phiarc/gui/MtowConfigurationManager.java
+++ src/client/java/org/phiarc/gui/MtowConfigurationManager.java
@@ -9,7 +9,12 @@ public class MtowConfigurationManager {
     public MtowProtocol protocolStatus = MtowProtocol.TCP;
     public List<String> proxyList = new ArrayList<>();
     public String password = "";
-
+    public int activeAmount = 3;
+    public int alternativeAmount = 5;
+    public int reuseAmount = 2;
+    public int resendIncrement = 1;
+    public int activeInterval = 5;
+    public int alternativeInterval = 30;
 
     private final String PREFIX = "@mtow::";
 
@@ -23,34 +28,35 @@ public class MtowConfigurationManager {
                 proxyList.forEach(string -> builder.append(string).append(";"));
                 builder.setLength(builder.length() - 1);
             }
-            builder.append(",").append(password);
+            builder.append(",")
+                    .append(password).append(",")
+                    .append(activeAmount).append(",")
+                    .append(alternativeAmount).append(",")
+                    .append(reuseAmount).append(",")
+                    .append(resendIncrement).append(",")
+                    .append(activeInterval).append(",")
+                    .append(alternativeInterval);
 
             return builder.toString();
         }
         return uri;
     }
 
-    private MtowConfigurationManager() {}
-
-    public MtowConfigurationManager copy() {
-        MtowConfigurationManager manager = new MtowConfigurationManager();
-        manager.proxyStatus = this.proxyStatus;
-        manager.protocolStatus = this.protocolStatus;
-        manager.proxyList = new ArrayList<>(this.proxyList);
-        manager.password = this.password;
-        return manager;
-    }
-
     public MtowConfigurationManager(String string) {
         if (string.startsWith(PREFIX)) {
-            String[] contents = string.substring(PREFIX.length()).split(",", 4);
+            String[] contents = string.substring(PREFIX.length()).split(",", 10);
             try {
                 this.proxyStatus = replace(MtowStatus.create(limit(Integer.parseInt(contents[0]), 2)), this.proxyStatus);
                 this.protocolStatus = replace(MtowProtocol.create(limit(Integer.parseInt(contents[1]), 1)), this.protocolStatus);
                 this.proxyList = replace(Arrays.asList(contents[2].split(";")), this.proxyList);
                 this.password = replace(contents[3], this.password);
-            } catch (Exception ignored) {
-            }
+                this.activeAmount = replace(Math.max(Integer.parseInt(contents[4]), 0), this.activeAmount);
+                this.alternativeAmount = replace(Math.max(Integer.parseInt(contents[5]), 0), this.alternativeAmount);
+                this.reuseAmount = replace(Math.max(Integer.parseInt(contents[6]), 0), this.reuseAmount);
+                this.resendIncrement = replace(Math.max(Integer.parseInt(contents[7]), 0), this.resendIncrement);
+                this.activeInterval = replace(Math.max(Integer.parseInt(contents[8]), 0), this.activeInterval);
+                this.alternativeInterval = replace(Math.max(Integer.parseInt(contents[9]), 0), this.alternativeInterval);
+            } catch (Exception ignored) {}
         }
     }
 
--- src/client/java/org/phiarc/gui/screen/MtowProxyRulesScreen.java
+++ src/client/java/org/phiarc/gui/screen/MtowProxyRulesScreen.java
@@ -1,24 +1,82 @@
 package org.phiarc.gui.screen;
 
 import net.minecraft.client.gui.components.AbstractWidget;
+import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.client.gui.layouts.HeaderAndFooterLayout;
+import net.minecraft.client.gui.layouts.LinearLayout;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 
 public class MtowProxyRulesScreen extends Screen {
-    private final Screen lastScreen;
+    private final MtowConfigurationScreen lastScreen;
 
     private final static Component TITLE = Component.translatable("mtow.screen.rule_title");
+    private final static Component ACTIVE_AMOUNT = Component.translatable("mtow.network.active_amount");
+    private final static Component ALTERNATIVE_AMOUNT = Component.translatable("mtow.network.alternative_amount");
+    private final static Component REUSE_AMOUNT = Component.translatable("mtow.network.reuse_amount");
+    private final static Component RESEND_INCREMENT = Component.translatable("mtow.network.resend_increment");
+    private final static Component ACTIVE_INTERVAL = Component.translatable("mtow.network.active_interval");
+    private final static Component ALTERNATIVE_INTERVAL = Component.translatable("mtow.network.alternative_interval");
+
+    private EditBox activeAmountEdit;
+    private EditBox alternativeAmountEdit;
+    private EditBox reuseAmountEdit;
+    private EditBox resendIncrementEdit;
+    private EditBox activeIntervalEdit;
+    private EditBox alternativeIntervalEdit;
 
     private final HeaderAndFooterLayout layout = new HeaderAndFooterLayout(this, 61, 33);
 
-    protected MtowProxyRulesScreen(Screen screen) {
+    protected MtowProxyRulesScreen(MtowConfigurationScreen screen) {
         super(TITLE);
         lastScreen = screen;
     }
 
+    /*
+        activeAmountEdit.;
+        alternativeAmountEdit.;
+        reuseAmountEdit.;
+        resendIncrementEdit.;
+        activeIntervalEdit.;
+        alternativeIntervalEdit.;
+     */
+
     public void init() {
         this.layout.addTitleHeader(TITLE, this.font);
+
+        activeAmountEdit = new EditBox(this.font, 100, 20, ACTIVE_AMOUNT);
+        alternativeAmountEdit = new EditBox(this.font, 100, 20, ALTERNATIVE_AMOUNT);
+        reuseAmountEdit = new EditBox(this.font, 100, 20, REUSE_AMOUNT);
+        resendIncrementEdit = new EditBox(this.font, 100, 20, RESEND_INCREMENT);
+        activeIntervalEdit = new EditBox(this.font, 100, 20, ACTIVE_INTERVAL);
+        alternativeIntervalEdit = new EditBox(this.font, 100, 20, ALTERNATIVE_INTERVAL);
+        activeAmountEdit.setHint(ACTIVE_AMOUNT);
+        alternativeAmountEdit.setHint(ALTERNATIVE_AMOUNT);
+        reuseAmountEdit.setHint(REUSE_AMOUNT);
+        resendIncrementEdit.setHint(RESEND_INCREMENT);
+        activeIntervalEdit.setHint(ACTIVE_INTERVAL);
+        alternativeIntervalEdit.setHint(ALTERNATIVE_INTERVAL);
+        /*
+        activeAmountEdit.setValue(String.valueOf(this.lastScreen.configuration.activeAmount));
+        alternativeAmountEdit.setValue(String.valueOf(this.lastScreen.configuration.alternativeAmount));
+        reuseAmountEdit.setValue(String.valueOf(this.lastScreen.configuration.reuseAmount));
+        resendIncrementEdit.setValue(String.valueOf(this.lastScreen.configuration.resendIncrement));
+        activeIntervalEdit.setValue(String.valueOf(this.lastScreen.configuration.activeInterval));
+        alternativeIntervalEdit.setValue(String.valueOf(this.lastScreen.configuration.alternativeInterval));
+         */
+        LinearLayout linearLayout1 = ((LinearLayout)this.layout.addToHeader(LinearLayout.vertical())).spacing(4);
+        LinearLayout linearLayout2 = ((LinearLayout)this.layout.addToHeader(LinearLayout.vertical())).spacing(4);
+        linearLayout1.addChild(activeAmountEdit);
+        linearLayout2.addChild(alternativeAmountEdit);
+        linearLayout1.addChild(reuseAmountEdit);
+        linearLayout2.addChild(resendIncrementEdit);
+        linearLayout1.addChild(activeIntervalEdit);
+        linearLayout2.addChild(alternativeIntervalEdit);
+        LinearLayout linearLayout = ((LinearLayout)this.layout.addToHeader(LinearLayout.horizontal())).spacing(4);
+        linearLayout.addChild(linearLayout1);
+        linearLayout.addChild(linearLayout2);
+
+        this.layout.addToContents(linearLayout);
 
         this.layout.visitWidgets((guiEventListener) -> {
             AbstractWidget var10000 = (AbstractWidget)this.addRenderableWidget(guiEventListener);
@@ -31,6 +89,14 @@ public class MtowProxyRulesScreen extends Screen {
     }
 
     public void onClose() {
+        try {
+            this.lastScreen.configuration.activeAmount = Integer.parseInt(activeAmountEdit.getValue());
+            this.lastScreen.configuration.alternativeAmount = Integer.parseInt(alternativeAmountEdit.getValue());
+            this.lastScreen.configuration.reuseAmount = Integer.parseInt(reuseAmountEdit.getValue());
+            this.lastScreen.configuration.resendIncrement = Integer.parseInt(resendIncrementEdit.getValue());
+            this.lastScreen.configuration.activeInterval = Integer.parseInt(activeIntervalEdit.getValue());
+            this.lastScreen.configuration.alternativeInterval = Integer.parseInt(alternativeIntervalEdit.getValue());
+        } catch (Exception ignored) {}
         assert this.minecraft != null;
         this.minecraft.setScreen(this.lastScreen);
     }
